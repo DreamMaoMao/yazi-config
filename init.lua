606c540f8d1ea10b845fb6e2e5016b39a5acc6bb
DreamMaoMao <2523610504@qq.com>
2024-07-16 13:35:45 +0800

    update

--- init.lua
+++ init.lua
@@ -1,8 +1,13 @@
-require("keyjump"):setup {
-	icon_fg = "#fda1a1",
-}
+-- require("keyjump"):setup({
+-- 	icon_fg = "#fda1a1",
+-- })
 
-require("searchjump"):setup {
+require("easyjump"):setup({
+	icon_fg = "#fda1a1",
+    first_key_fg = "#df6249"
+})
+
+require("searchjump"):setup({
 	unmatch_fg = "#b2a496",
     match_str_fg = "#000000",
     match_str_bg = "#73AC3A",
@@ -14,21 +19,23 @@ require("searchjump"):setup {
     show_search_in_statusbar = false,
     auto_exit_when_unmatch = true,
     enable_capital_lable = true,
-	search_patterns = {"%d+.1080p","第%d+集","%.E%d+","S%d+E%d+",}
-}
+	search_patterns = ({"%d+.1080p","第%d+集","%.E%d+","S%d+E%d+",})
+})
 
-require("custom"):setup{
+require("custom"):setup({
 	border_color = "#9a8774",
 	user_color = "#D4BB91"
-}
+})
 
-require("git-status"):setup{
+require("git-status"):setup({
     style = "linemode" -- beside or linemode
-}
+})
 
-require("current-size"):setup{
-    folder_size_ignore = {"/home/wrq","/"},
-}
--- require("zoxide"):setup {
+require("current-size"):setup({
+    folder_size_ignore = ({"/home/wrq","/","/home"}),
+})
+
+-- require("zoxide"):setup({
 -- 	update_db = true,
--- }
+-- })
+
